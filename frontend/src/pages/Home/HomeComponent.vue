90db2815e5e6af47169dbd59c27276efa64e3da7
Marco <marco_12aurelio@hotmail.com>
2021-10-20 18:58:48 -0300

    Adiciona criação de rotas de login e home - VueRouter

---
+++ frontend/src/pages/Home/HomeComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="home">
+    <h1>Título</h1>
+    <p>Tela referente a Home </p>
+    </div>
+    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ frontend/src/pages/Login/LoginComponent.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="login">
+    <h1>Título</h1>
+    <p>Tela referente ao login do sistema</p>
+</div>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
