f54dd96b1578953142c62cd55422ed6f5971a566
Marco <marco_12aurelio@hotmail.com>
2021-10-27 15:41:38 -0300

    Adiciona topo da SideBar

--- frontend/src/pages/DashBoard/components/SideBarComponent.vue
+++ frontend/src/pages/DashBoard/components/SideBarComponent.vue
@@ -1,13 +1,20 @@
 <template>
-    <div>
-        <p>SideBar</p>
-        <i class="fas fa-user-circle"></i>
+    <div class="sidebar">
+        <div class="top">
+            <div class="avatar">
+                <span>
+                    <i class="fas fa-user-circle"></i>
+                </span>
+            </div>
+            <div class="info">
+                <p class="name">Marco Aurélio</p>
+                <p class="status">online</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-
-
 export default {
     name: 'SideBarComponent',
 
@@ -15,5 +22,35 @@ export default {
 </script>
 
 <style lang="scss">
+    .sidebar {
+        color: white;
+
+        .top{
+            width: 100%;
+            display: flex;
+            justify-content: center;
+            padding: 25px 5px;
+
+            .avatar{
+                margin-right: 10px;
+
+                span{
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    font-size: 37px;       
+                }
+            }
+            
+            .info {
+                .name {
+                    margin: 0;
+                }
+                .status {
+                    font-size: 12px;
+                }
+            }
+        }
+    }
 
 </style>
